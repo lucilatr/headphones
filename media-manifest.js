31c220894feeed926f92b612a7b0b6343b096be4
lucilatr <lucilatrobbiani@gmail.com>
2026-09-22 18:36:14 -0300

    Publish Headphones world guide (ES/EN)

---
+++ media-manifest.js
@@ -0,0 +1,57 @@
+window.MEDIA_MANIFEST = {
+  "characters/dj-sparkle": [
+    "images/characters/DJ SPARKLE/dj-sparkle-01.png"
+  ],
+  "characters/evie": [
+    "images/characters/EVIE/evie-01.png"
+  ],
+  "characters/fonk": [
+    "images/characters/FONK/fonk-01.png"
+  ],
+  "characters/fuse": [
+    "images/characters/FUSE/Tuning_in-Fuse_v01_hair-style_02_turnaround.png",
+    "images/characters/FUSE/Tuning_in-Fuse_v01_updated-guitar.png"
+  ],
+  "characters/harmony": [
+    "images/characters/HARMONY/harmony-01.png"
+  ],
+  "characters/kay": [
+    "images/characters/KAY/01-strip.png",
+    "images/characters/KAY/02-nurse.png"
+  ],
+  "characters/mama-hallelujah": [
+    "images/characters/MAMA HALLELUJAH/mama-hallelujah-01.png"
+  ],
+  "characters/mc-flow": [
+    "images/characters/MC FLOW/mc-flow-01.png"
+  ],
+  "characters/miles": [
+    "images/characters/MILES/miles-01.png",
+    "images/characters/MILES/miles-02.png"
+  ],
+  "characters/pip-pop": [
+    "images/characters/PIP-POP/pip-pop-01.png",
+    "images/characters/PIP-POP/pip-pop-02.png"
+  ],
+  "characters/swing": [
+    "images/characters/SWING/swing-01.png",
+    "images/characters/SWING/swing-02.png"
+  ],
+  "characters/teller": [
+    "images/characters/TELLER/teller-01.png"
+  ],
+  "characters/twang": [
+    "images/characters/TWANG/twang-01.png"
+  ],
+  "characters/zoe": [
+    "images/characters/ZOE/01-present.png",
+    "images/characters/ZOE/02-ages.png"
+  ],
+  "environments/harmonic-hall": [
+    "images/environments/HARMONIC HALL/enhanced_Harmonic_Hall_epic_02.png"
+  ],
+  "environments/metal-mountain": [
+    "images/environments/METAL MOUNTAIN/Tuning_in_RockLand_styleframe_01B.png",
+    "images/environments/METAL MOUNTAIN/Tuning_in_RockLand_styleframe_02B.png"
+  ]
+};
